cc5af2786cfc997cff7582bf641e443b4a25128f
DanWeber-jun <dimkauric@gmail.com>
2021-06-03 07:33:13 +0200

    Сделал правки

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-const getSomeInt = ((min,max) => {
+const getSomeInt = (min,max) => {
   if (min < 0 || max < 0 ) {
     return -1;
   }
   if (min > max) {
-    [min,max] = [max-min];
+    [min,max] = [max,min];
   }
   return Math.random(Math.random() * (max - min)) + min;
-});
+};
 
 getSomeInt(0, 100);
 
-const getSomeNumber = ((min, max) => {
+const getSomeNumber = (min, max) => {
   if (min < 0 || max < 0 ) {
     return -1;
   }
   if (min > max) {
-    [min,max] = [max-min];
+    [min,max] = [max,min];
   }
-  return Math.random() * (max - min) + min.toFixed();
-});
+  return (Math.random() * (max - min) + min).toFixed();
+};
 
 getSomeNumber(0, 100);
