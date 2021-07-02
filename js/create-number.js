fd37ca30d713311fadbc579f1bf56c055e17c21f
DanWeber-jun <dimkauric@gmail.com>
2021-07-02 23:58:06 +0200

    ДЗ Отрисуй меня полностью

--- js/create-number.js
+++ js/create-number.js
@@ -8,28 +8,83 @@ const types = {
   hotel: 'Отель',
 };
 
+
+const createFeatures = (features, container) => {
+  const list = container.querySelectorAll('li');
+  if (list) {
+    list.forEach((item) => {
+      if (features.indexOf(item.classList[1].replace('popup__feature--', '')) === -1) {
+        item.remove();
+      }
+    });
+  }
+};
+
+
 const createCard = ({offer}) => {
   const cloneTemplate = template.cloneNode(true);
-  cloneTemplate.querySelector('.popup__title').textContent = offer.title;
-  cloneTemplate.querySelector('.popup__text--address').textContent = offer.address;
-  cloneTemplate.querySelector('.popup__text--price').textContent = ` ${offer.price} ₽/ночь`;
-  cloneTemplate.querySelector('.popup__type').textContent = types[offer.type];
-  cloneTemplate.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests}`;
-  cloneTemplate.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-  const container = cloneTemplate.querySelector('.popup__features');
-  const createFeatures = (features, container) => {
-    const list = container.querySelector('li');
-    if (list) {
-      list.forEach((item) => {
-        if (features.indexOf(item.classList[1].replace('popup__feature--', '')) === -1) {
-          item.remove();
-        }
-      });
-    }
-  };
-  createFeatures(features, container);
+  const title = cloneTemplate.querySelector('.popup__title');
+  if (offer.title) {
+    title.textContent = offer.title;
+  } else {
+    title.remove();
+  }
 
-  cloneTemplate.querySelector('.popup__description').textContent = offer.description;
+  const addres = cloneTemplate.querySelector('.popup__text--address');
+  if (offer.addres) {
+    addres.textContent = offer.address;
+  } else {
+    addres.remove();
+  }
+
+  const price = cloneTemplate.querySelector('.popup__text--price');
+  if (offer.price) {
+    price.textContent = ` ${offer.price} ₽/ночь`;
+  } else {
+    price.remove();
+  }
+
+  const type = cloneTemplate.querySelector('.popup__type');
+  if (offer.type) {
+    type.textContent = types[offer.type];
+  } else {
+    type.remove();
+  }
+
+  const rooms = cloneTemplate.querySelector('.popup__text--capacity');
+  if (offer.rooms) {
+    rooms.textContent = `${offer.rooms} комнаты для ${offer.guests}`;
+  } else {
+    rooms.remove();
+  }
+
+  const time = cloneTemplate.querySelector('.popup__text--time');
+  if (offer.checkin && offer.checkout) {
+    time.textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+  } else {
+    time.remove();
+  }
+
+  const description = cloneTemplate.querySelector('.popup__description');
+  if (offer.description) {
+    description.textContent = offer.description;
+  } else {
+    description.remove();
+  }
+  createFeatures(offer.features, cloneTemplate.querySelector('.popup__features'));
+
+  const photos = cloneTemplate.querySelector('.popup__photos');
+  if (offer.photos) {
+    const photoElement = photos.querySelector('img');
+    photos.innerHTML = '';
+    offer.photos.forEach((url) => {
+      photoElement.src = url;
+      photos.appendChild(photoElement);
+    });
+  } else {
+    photos.remove();
+  }
+
   return cloneTemplate;
 };
 
--- js/data.js
+++ js/data.js
@@ -31,40 +31,37 @@ const TIMES = ['12:00', '13:00', '14:00'];
 const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 
 
-const addOffer = (index) => {
-  return {
-    author: {
-      avatar:`img/avatars/user${index < COUNT ? '0' : `${  index}`}.png`,
-    },
-    offer: {
-      title: `Моя уютная обитель${COUNT[index]}`,
-      address: location.lat + location.lng,
-      price: getRandomNumber(Price.MIN, Price.MAX),
-      type: TYPES[getSomeInt(0, TYPES.length - 1)],
-      rooms: getRandomNumber(Rooms.MIN, Rooms.MAX),
-      guests: getRandomNumber(Guests.MIN, Guests.MAX),
-      checkin: TIMES[getSomeInt(0, TIMES.length - 1)],
-      checkout: TIMES[getSomeInt(0, TIMES.length - 1)],
-      features: shuffleArray(FEATURES).slice(getSomeInt(0, FEATURES.length - 1)),
-      description: 'Комфорт, уют, чистота и порядок - здесь именно то место, которое вы искали для отдыха, чтобы чувствовать себя как дома вдали от него!',
-      photos: shuffleArray(PHOTOS).slice(getSomeInt(0, PHOTOS.length - 1)),
-    },
-    location: {
-      lat: getSomeFloat(Lat.MIN, Lat.MAX, Lat.DIGIT),
-      lng: getSomeFloat(Lng.MIN, Lng.MAX, Lng.DIGIT),
-    },
-  }
-};
+const addOffer = (index) =>  ({
+  author: {
+    avatar:`img/avatars/user${index < COUNT ? '0' : `${  index}`}.png`,
+  },
+  offer: {
+    title: `Моя уютная обитель${COUNT[index]}`,
+    address: location.lat + location.lng,
+    price: getRandomNumber(Price.MIN, Price.MAX),
+    type: TYPES[getSomeInt(0, TYPES.length - 1)],
+    rooms: getRandomNumber(Rooms.MIN, Rooms.MAX),
+    guests: getRandomNumber(Guests.MIN, Guests.MAX),
+    checkin: TIMES[getSomeInt(0, TIMES.length - 1)],
+    checkout: TIMES[getSomeInt(0, TIMES.length - 1)],
+    features: shuffleArray(FEATURES).slice(getSomeInt(0, FEATURES.length - 1)),
+    description: 'Комфорт, уют, чистота и порядок - здесь именно то место, которое вы искали для отдыха, чтобы чувствовать себя как дома вдали от него!',
+    photos: shuffleArray(PHOTOS).slice(getSomeInt(0, PHOTOS.length - 1)),
+  },
+  location: {
+    lat: getSomeFloat(Lat.MIN, Lat.MAX, Lat.DIGIT),
+    lng: getSomeFloat(Lng.MIN, Lng.MAX, Lng.DIGIT),
+  },
+});
 
 const getOffers = () => {
-  for(let i = 0; i < COUNT.lenght; i++) {
+  for(let i = 0; i < COUNT; i++) {
     offers.push(addOffer(i));
   }
 };
 
 getOffers();
 addOffer();
-console.log(offers);
 
-export{offers};
+export {offers};
 
